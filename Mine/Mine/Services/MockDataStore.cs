b5d2d33c31facb39045954746fd737b6424af0f2
Sam Borhan <samborhan1566@gmail.com>
2021-01-18 21:07:51 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Hot Sauce", Description="Causes enemy goes blind and misses to attack.", Value = 6 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sticky Noddle", Description="Disables enemy to move.", Value = 4 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Pan", Description="Reduces 3 points of health", Value = 8 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Mosquito Nose", Description="Makes enemey's skin itches uncontorllably till drops a special weapon to get relief.", Value = 10 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Flip Flops", Description="A ranged weapon,it can target anything by 3 blocks away also it will resduce 3 points of health.", Value = 9 },
             };
         }
 
