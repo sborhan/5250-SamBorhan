5b3ca06d1ae371e66d4401a1972319906edd9160
Sam Borhan <samborhan1566@gmail.com>
2021-02-04 16:47:20 -0800

    Added RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15() to RollDiceHelperUnitTest.cs

--- UnitTests/Helpers/RollDiceHelperUnitTest.cs
+++ UnitTests/Helpers/RollDiceHelperUnitTest.cs
@@ -128,6 +128,24 @@ namespace UnitTests.Models
         }
 
 
+        [Test]
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+            // Act
+            var result = DiceHelper.RollDice(3, 10);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert 
+            Assert.AreEqual(15, result);
+        }
+
+
     }
 }
 
