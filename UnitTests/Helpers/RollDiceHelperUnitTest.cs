d4f796776572b4731aa33adb47876bce84922aa4
Sam Borhan <samborhan1566@gmail.com>
2021-02-04 16:23:41 -0800

    Added RollDice_Invalid_Dice_Zero_Should_Return_Zero() to RollDiceHelperUnitTest.cs

--- UnitTests/Helpers/RollDiceHelperUnitTest.cs
+++ UnitTests/Helpers/RollDiceHelperUnitTest.cs
@@ -52,6 +52,20 @@ namespace UnitTests.Models
             // Assert 
             Assert.AreEqual(1, result);
         }
+
+        [Test]
+        public void RollDice_Invalid_Dice_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
     }
 }
 
